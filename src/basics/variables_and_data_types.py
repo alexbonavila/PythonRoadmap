e4e7bd678f00185fb4ad0e73bf591c08819ce747
alexbonavila <alexbonavila44@gmail.com>
2023-10-31 20:03:01 +0100

    finxing bugs and screenshot add

--- src/basics/variables_and_data_types.py
+++ src/basics/variables_and_data_types.py
@@ -1,38 +1,51 @@
 # Integer
 age = 30
+print(age)
 
 # Float
 temperature = 98.6
+print(temperature)
 
 # String
 name = "Alice"
+print(name)
 
 # Boolean
 is_active = True
+print(is_active)
 
 # List
 fruits = ["apple", "banana", "cherry"]
+print(fruits)
 
 # Tuple
 coordinates = (10.0, 20.0)
+print(coordinates)
 
 # Set
 unique_numbers = {1, 2, 3, 4, 5}
+print(unique_numbers)
 
 # Dictionary
 person = {"name": "Bob", "age": 25}
+print(person)
 
 # NoneType (represents "null" or "no value")
 nothing = None
+print(nothing)
 
 # Bytes
 binary_data = b'\xDE\xAD\xBE\xEF'
+print(binary_data)
 
 # bytearray
 byte_array = bytearray(5)
+print(byte_array)
 
-# memoryview
+# memory view
 memory_view = memoryview(bytearray("hello", "utf-8"))
+print(memory_view)
 
 # Complex (for complex numbers)
 complex_number = 3 + 5j
+print(complex_number)
